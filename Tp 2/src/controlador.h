362eb0a36fd1d024591105fd53a8e13720b4ce7d
FrancoVivas <68014747+FrancoVivas@users.noreply.github.com>
2021-07-06 17:16:37 -0300

    Add files via upload

---
+++ Tp 2/src/controlador.h
@@ -0,0 +1,45 @@
+/*
+ * controlador.h
+ *
+ *  Created on: 6 jul. 2021
+ *      Author: N3630024213
+ */
+
+#ifndef CONTROLADOR_H_
+#define CONTROLADOR_H_
+
+/** \brief shows the menu
+ *
+ * \return int Return the selected option
+ *
+ */
+int Menu();
+
+/** \brief generates an Id automatically
+ *
+ * \param void
+ * \return int
+ *
+ */
+int CalculateEmployeeId(void);
+
+
+void getString(char [],char input[]);
+float getFloat(char* );
+int getInt(char* mensaje);
+
+
+
+int isOnlyLetters(char str[]);
+int isNumericFloat(char str[]);
+int isNumeric(char *str);
+
+int getStringNumericFloat(char message[],char input[]);
+int getStringNumeros(char mensaje[],char input[]);
+int getStringLetras(char mensaje[],char input[]);
+
+int getValidInt(char requestMessage[],char errorMessage[], int* input,int lowLimit, int hiLimit,int attemps);
+int getValidFloat(char requestMessage[],char errorMessage[], float* input,float lowLimit, float hiLimit,int attemps);
+int getValidString(char requestMessage[],char errorMessage[], char errorMessageLenght[],char input[], int maxLenght,int attemps);
+
+#endif /* CONTROLADOR_H_ */
